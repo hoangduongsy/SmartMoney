f3129265b62ef0e8320070dde723fac7b25cda30
duongsyhoang <duongsyhoang@gmail.com>
2020-11-09 16:02:31 +0700

    finish heaeder

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,3 +3,12 @@ $('.dropdown').on('click', function(e) {
     var dropdown = target.closest('.dropdown');
     return !dropdown.hasClass('open') || !target.hasClass('dropdown-menu__search__input');
 });
+
+/* More button toggle */
+function classToggle() {
+    const navs = document.querySelectorAll('.Navbar__Items')
+    
+    navs.forEach(nav => nav.classList.toggle('Navbar__ToggleShow'));
+  }
+  document.querySelector('.Navbar__Link-toggle')
+    .addEventListener('click', classToggle);
